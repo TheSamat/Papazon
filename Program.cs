2318e241a4f2891bf708b6ddeddc2629de691bb6
TheSamat <seitbekuulusalavat@gmail.com>
2022-12-20 12:07:27 +0600

    update

--- Program.cs
+++ Program.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 
 public class Program
 {
-    private static void Main(string[] args)
-    {
-        EventHandler<EventArgs> h;
-    }
+}
+
+class Base
+{
+    public virtual void M<T1, T2>() 
+        where T1:struct 
+    {    }
+}
+sealed class Derived : Base
+{
+    public override void M<T3, T2>()
+        where T2: class
+    {    }
 }
